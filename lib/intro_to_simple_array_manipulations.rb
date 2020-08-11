eb256e0c7575a9b7674f262e502c7806b6eef0b4
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-11 17:57:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,8 @@
-def using_concat(my_favorite_things, all_my_favs)
-  (my_favorite_things).concat(all_my_favs)
+def using_concat(my_favorite_things, more_favs)
+  (my_favorite_things).concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages,)
+def using_insert
 end
 
 def using_uniq
